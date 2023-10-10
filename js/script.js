f7779700f031842329795271bc6d0ee9db393b19
elyonaya <elyona.laurent1@gmail.com>
2023-10-10 10:51:03 +0200

    ajout function changeColor

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
 let compteur = 0;
 
+function changeColor() {
+    if(compteur < 0) {
+        document.getElementById("counter").style.color = "red"
+      } else  if(compteur > 0) {
+        document.getElementById("counter").style.color = "green"
+      } else if(compteur == 0) {
+        document.getElementById("counter").style.color = "black"
+      }
+}
+
 function doDecrease() {
   compteur = compteur - 1;
+  changeColor()
   document.getElementById("counter").innerHTML = compteur;
 }
 
 function reset() {
   compteur = 0;
+  changeColor()
   document.getElementById("counter").innerHTML = compteur;
 }
 
 function doIncrease() {
   compteur = compteur + 1;
+  changeColor()
   document.getElementById("counter").innerHTML = compteur;
 }
 
 document.getElementById("decrease").addEventListener("click", doDecrease);
 document.getElementById("reset").addEventListener("click", reset);
 document.getElementById("increase").addEventListener("click", doIncrease);
+
